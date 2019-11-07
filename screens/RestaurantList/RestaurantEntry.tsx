90a4a8de205a971f4d06d26b554965e17755c63f
Alejandro Pacheco <alepacheco99@gmail.com>
2019-11-07 23:48:41 +0100

    Adding restaurant list

---
+++ screens/RestaurantList/RestaurantEntry.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 18px 8px;
+`;
+
+const TextBox = styled.View`
+  flex: 2;
+  display: flex;
+`;
+
+const RestaurantImage = styled.Image`
+  height: 84px;
+  width: 84px;
+  border-radius: 6px;
+`;
+
+const Title = styled.Text`
+  justify-content: center;
+  font-size: 24px;
+`;
+
+const SubTitle = styled.Text`
+  justify-content: center;
+  font-size: 18px;
+  color: gray;
+`;
+
+export const RestaurantEntry: React.FC<{ title: string }> = ({ title }) => {
+  return (
+    <Container>
+      <RestaurantImage source={{ uri: 'https://picsum.photos/50/50' }} />
+      <TextBox>
+        <Title>{title}</Title>
+        <SubTitle>This is the subtitle</SubTitle>
+      </TextBox>
+    </Container>
+  );
+};
--- screens/RestaurantList/index.tsx
+++ screens/RestaurantList/index.tsx
@@ -1,11 +1,56 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, Text, FlatList } from 'react-native';
+import { RestaurantEntry } from './RestaurantEntry';
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c5453605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f54534-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+  {
+    id: '534555-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c5354605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-5345345471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+  {
+    id: 'bd7acbea-c345341b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-54543c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '5844-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
 
 export class RestaurantList extends React.Component<{}> {
   render() {
     return (
       <SafeAreaView>
         <Text>List</Text>
+        <FlatList
+          data={DATA}
+          renderItem={({ item }) => <RestaurantEntry title={item.title} />}
+          keyExtractor={item => item.id}
+        />
       </SafeAreaView>
     );
   }
