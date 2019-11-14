7ab1d382b76e6ea7609864852095ec54af0b8933
Alejandro Pacheco <alepacheco99@gmail.com>
2019-11-14 15:44:17 +0000

    Fix getProfile

--- screens/Profile/utils.ts
+++ screens/Profile/utils.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import { API_URL } from '../../constants/network';
 
-export const getProfile = async ({ userId }: any) => {
+export const getProfile = async ({ sessionId }: any) => {
   const { data } = await axios.get(`${API_URL}/user`, {
     params: {
-      userId,
+      sessionId,
     },
   });
 
